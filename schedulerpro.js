c856a03db5d5a9d2eebaec920146742d27262e56
Ben Iofel <iofelben@gmail.com>
2016-04-02 16:18:21 -0400

    show the number of students interested

--- schedulerpro.js
+++ schedulerpro.js
@@ -1,10 +1,19 @@
-
-var timerid = setInterval(function() { //Start a timer to continually check if 'session' class divs are loaded
-    if($($(".session")[0]).text().substr(0,1) != "*") //Adds to any "session" without asterisk
-    {
-        $(".session").prepend("<b>*hello world</b>: ");
-    }
-}, 50);
+chrome.runtime.sendMessage({giveMe: 'user'}, function(response) {
+    var user = response.user
+    setInterval(function() { //Start a timer to continually check if 'session' class divs are loaded
+        var divs = $('.session')
+        for (let i=0; i<divs.length; ++i) {
+            let curDiv = divs[i]
+            if ($(curDiv).text()[0] != '*') { // update
+                let title = divs[i].title
+                let classid = title.substring(title.length-5, title.length)
+                $.getJSON('https://iofel.me:3000/api/interested/' + classid, function (data) {
+                    $(curDiv).prepend("<b>* " + data.count + " students are interested *</b><br>")
+                })
+            }
+        }
+    }, 50);
+})
 
 
 $(window).on('hashchange', function() {
