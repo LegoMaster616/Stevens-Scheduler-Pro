72c324cc3c51d095491a676cc1bf00ee7929bdf2
LegoMaster616 <andrew.afflitto@gmail.com>
2016-04-02 17:20:34 -0400

    Added code to registration.js

---
+++ registration.js
@@ -0,0 +1,24 @@
+alert("hi");
+/*
+$.getJSON('https://iofel.me:3000/api/interested/', function (data) {
+    
+})
+
+
+
+chrome.runtime.sendMessage({giveMe: 'user'}, function(response) {
+    var user = response.user;
+    
+    
+    
+    $("#firstclass").val(user[0]);
+    $("#secondclass").val(user[1]);;
+    $("#thirdclass").val(user[2]);;
+    $("#fourthclass").val(user[3]);;
+    $("#fifthclass").val(user[4]);;
+    $("#sixthclass").val(user[5]);;
+    $("#seventhclass").val(user[6]);;
+    $("#eighthclass").val(user[7]);;
+})
+
+*/
